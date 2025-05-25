d6eb1dbae3684e092767896aad8147e30bf0f519
Kiruthikadevi <37093551+Kiruthikapanner@users.noreply.github.com>
2025-05-24 17:42:48 -0700

    Add files via upload
    Week1 Assignments

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,24 @@
+package week1.day1;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float version=91.0f;
+		System.out.println("The value of version-type float is "+ version);
+		
+		String developer="Google";
+		System.out.println("The value of developer-type String is "+developer);
+		
+		boolean isBeta=false;
+		System.out.println("The value of isBeta-type boolean is "+isBeta);
+		
+		int releaseYear=2008;
+		System.out.println("The value of releaseYear-type int is "+releaseYear);
+		
+		char shortcutKey='C';
+		System.out.println("The value of shortcutKey-type char is "+shortcutKey);
+	}
+
+}
